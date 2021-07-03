a7fbe2dd5ce327304c35109a6cad285043b096ff
Diego López <diego.lopezgarcia@hotmail.com>
2021-07-03 16:51:53 +0200

    feat: Added a simple clock with the current time

--- src/App.svelte
+++ src/App.svelte
@@ -1,19 +1,22 @@
 <script>
   import { getTopWallpaper } from "./services/reddit.js";
   import { onMount } from "svelte";
+  import Clock from "./Clock.svelte";
 
   let bg = null;
 
   onMount(async () => {
     const bgResponse = await getTopWallpaper();
     bg = bgResponse.data.children[0].data.url;
-    console.log(bg);
   });
 
 </script>
 
 <main class="overlay">
   <div class="bg" style="background-image: url('{bg}');">
+    <div class="clock-container">
+      <Clock />
+    </div>
   </div>
 </main>
 
@@ -33,6 +36,11 @@
     background-position: center;
     background-size: cover;
     animation: fadeIn 1s;
+    overflow: hidden;
+  }
+  .clock-container {
+    text-align: center;
+    margin-top: 10rem;
   }
   @keyframes fadeIn {
     0% {
---
+++ src/Clock.svelte
@@ -0,0 +1,38 @@
+<script>
+  import { getTopWallpaper } from "./services/reddit.js";
+  import { onMount } from "svelte";
+
+  let h = "00";
+  let m = "00";
+  let s = "00";
+
+  function startClock() {
+    const now = new Date();
+    h = addZero(now.getHours());
+    m = addZero(now.getMinutes());
+    s = addZero(now.getSeconds());
+  }
+
+  function addZero(number) {
+    if (number < 10) {
+      return "0" + number;
+    }
+    return number.toString();
+  }
+
+  onMount(async () => {
+    startClock();
+    setInterval(() => startClock(), 1 * 1000);
+  });
+
+</script>
+
+<h2 class="clock">{h}:{m}:{s}</h2>
+
+<style>
+  .clock {
+    color: white;
+    margin: 0;
+    font-size: 6rem;
+  }
+</style>
